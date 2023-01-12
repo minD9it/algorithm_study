048190d3168695d942402db8a0fce3c5690ebae4
minji <minji.kang0408@gmail.com>
2023-01-12 23:47:39 +0900

    20230112 baekjoon..;(

--- 1.Greedy/baekjoon_algorithm/p1931_meeting_room.py
+++ 1.Greedy/baekjoon_algorithm/p1931_meeting_room.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # 회의 수
 meeting = int(input())
 
@@ -5,14 +7,19 @@ meeting = int(input())
 meeting_time = []
 for i in range(meeting):
     meeting_time.append(list(map(int, input().split())))
+print(meeting_time)
+
+# 오름차순 정렬
+meeting_time = np.unique(meeting_time, axis=0)
+print(meeting_time)
 
 maximum_meeting = 0
 
-# 많은 회의를 진행하기 위해서는 회의 시간이 적은 회의를 우선순위로 하면 됨
-meeting_duration  = []
-for m in meeting_time:
-    meeting_duration.append(m[1] - m[0]) # 회의 시간 구하기
+start = meeting_time.T[0]
+print(start)
 
 
-# 마지막 회의 시간을 기준으로 계산하는 것으로 해보자 
-# numpy.unique를 사용하면 array 안에 리스트가 오름차순으로 정렬된다.
+# # 많은 회의를 진행하기 위해서는 회의 시간이 적은 회의를 우선순위로 하면 됨
+# meeting_duration  = []
+# for m in meeting_time:
+#     meeting_duration.append(m[1] - m[0]) # 회의 시간 구하기
