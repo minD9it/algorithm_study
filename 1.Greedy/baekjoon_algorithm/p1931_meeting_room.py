058e36fc7c041f9d92f6d2a66c285121f0384128
minji <minji.kang0408@gmail.com>
2023-03-05 14:36:52 +0900

    20230305 sorted key...!

--- 1.Greedy/baekjoon_algorithm/p1931_meeting_room.py
+++ 1.Greedy/baekjoon_algorithm/p1931_meeting_room.py
@@ -1,25 +1,33 @@
-import numpy as np
+# # 회의 수
+# meeting = int(input())
 
-# 회의 수
-meeting = int(input())
+# # 각 회의의 시작 시간과 끝나는 시간
+# meeting_time = []
+# for i in range(meeting):
+#     meeting_time.append(list(map(int, input().split())))
 
-# 각 회의의 시작 시간과 끝나는 시간
-meeting_time = []
-for i in range(meeting):
-    meeting_time.append(list(map(int, input().split())))
-print(meeting_time)
+# # 오름차순 정렬
+# meeting_time.sort()
 
-# 오름차순 정렬
-meeting_time = np.unique(meeting_time, axis=0)
-print(meeting_time)
+# # 내림차순 정렬
+# meeting_time.reverse()
 
-maximum_meeting = 0
+# current = meeting_time[0]
+# count = 1
+# for i, mt in enumerate(meeting_time[1:], 1):
+#     if meeting_time[i][1] <= current[0] and meeting_time[i][0] < current[1]: # 
+#         count += 1
+#         current = mt
 
-start = meeting_time.T[0]
-print(start)
+# print(count)
 
+n = int(input()) # 회의 개수
+time = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x:(x[1], x[0])) # key 우선순위 -> 끝나는 시간으로 정렬 ~ 시작 시간 정렬 ex) [1, 4], [2, 4] 이런식
+print(time)
 
-# # 많은 회의를 진행하기 위해서는 회의 시간이 적은 회의를 우선순위로 하면 됨
-# meeting_duration  = []
-# for m in meeting_time:
-#     meeting_duration.append(m[1] - m[0]) # 회의 시간 구하기
+ans = end = 0
+for s, e in time:
+    if s >= end:
+        ans += 1
+        end = e
+print(ans)
